e390b366794641e74bec92f28449364e8788c5b5
Jan Pfeifer <pfeifer@gmail.com>
2023-10-02 10:18:54 +0200

    Added nbexec to execute a notebook from the command line.

---
+++ cmd/nbexec/nbexec.go
@@ -0,0 +1,292 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/proto"
+	"github.com/janpfeifer/gonb/common"
+	"github.com/janpfeifer/must"
+	"io"
+	"k8s.io/klog/v2"
+	"net"
+	"os"
+	"os/exec"
+	"path"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var (
+	// panicf alias.
+	panicf = common.Panicf
+
+	// Extra arguments passed to `jupyter notebook` -- can be set multiple times.
+	flagJupyterExtraFlags = common.ArrayFlag{}
+
+	flagNotebook = flag.String("n", "", "Notebook to execute. Must be a relative path "+
+		"to the current directory (or --jupyter_dir if set) and cannot use \"..\", since it is going to "+
+		"be used in an URL.")
+	flagJupyterDir = flag.String("jupyter_dir", "",
+		"Directory where to execute `jupyter notebook`. If empty, it will use the current directory. "+
+			"The notebook path is relative to this directory.")
+	flagJupyterLog = flag.Bool("jupyter_log", false,
+		"Set to true and it will output to stdout everything output by `jupyter notebook`")
+
+	flagConsoleLog = flag.Bool("console_log", false,
+		"Set to true to capture and output what is written in the "+
+			"browser's console, using `console.log()` in javascript.")
+
+	flagScreenshot = flag.String("screenshot", "",
+		"PNG file where to save a screenshot of the page after the notebook is executed. "+
+			"If left empty no screenshots are made.")
+)
+
+func main() {
+	klog.InitFlags(nil)
+	defer klog.Flush()
+
+	flag.Var(&flagJupyterExtraFlags, "jupyter_args",
+		"Extra arguments passed to `jupyter notebook` -- can be set multiple times. "+
+			"`nbexec` always sets the following arguments: "+
+			"`--no-browser --expose-app-in-browser --ServerApp.port=<port>`.")
+	flag.Parse()
+
+	// Sanity checking.
+	if strings.Index(*flagNotebook, "/../") >= 0 ||
+		strings.Index(*flagNotebook, "../") == 0 ||
+		strings.Index(*flagNotebook, "/..") == len(*flagNotebook)-3 {
+		klog.Fatalf("Invalid value for notebook -n=%q: cannot use \"..\" in path")
+	}
+	notebookPath := *flagJupyterDir
+	if notebookPath == "" {
+		notebookPath = must.M1(os.Getwd())
+	}
+	notebookPath = path.Join(notebookPath, *flagNotebook)
+	if _, err := os.Stat(notebookPath); err != nil {
+		klog.Fatalf("The notebook given -n=%q resolves to %q, and I can't access it!? Error: %v")
+	}
+
+	// Start Jupyter Notebook
+	startJupyterNotebook()
+	if jupyterDone == nil || jupyterDone.Test() {
+		klog.Errorf("The program `jupyter notebook` failed to run, notebook was not executed!")
+		return
+	}
+
+	go func() {
+		// Start headless chromium with go-rod.
+		jupyterReady.Wait()
+		url := fmt.Sprintf("%snotebooks/%s?token=%s", jupyterBaseUrl, *flagNotebook, jupyterToken)
+		klog.V(1).Infof("URL: %s", url)
+		defer func() {
+			e := recover()
+			if !jupyterDone.Test() {
+				jupyterStop.Trigger()
+				if e != nil {
+					// Wait for jupyter notebook to get killed, before re-throwing the panic.
+					klog.Errorf("%v", e)
+					jupyterDone.Wait()
+					panic(e)
+				}
+			} else if e != nil {
+				panic(e)
+			}
+		}()
+		executeNotebook(url)
+	}()
+
+	// Wait for `jupyter notebook` to finish.
+	jupyterDone.Wait()
+}
+
+// Jupyter Notebook execution related variables.
+var (
+	jupyterCmd                             *exec.Cmd
+	jupyterExecPath                        string
+	jupyterReady, jupyterDone, jupyterStop *common.Latch
+
+	jupyterPort                  int
+	jupyterBaseUrl, jupyterToken string
+)
+
+// startJupyterNotebook will start Jupyter Notebook. It panics if something goes wrong.
+func startJupyterNotebook() {
+	// Find jupyter executable.
+	var err error
+	jupyterExecPath, err = exec.LookPath("jupyter")
+	if err != nil {
+		panicf(
+			"Command `jupyter` is not in path. `nbexec` needs access the `jupyter` binary -- and `notebook` "+
+				"subcommand installed (usually with something like `pip install notebook`, or something similar "+
+				"if using Conda. Error: %v", err)
+	}
+	klog.Infof("jupyter: found in %q", jupyterExecPath)
+
+	// Find an available port -- we could set the port to 0 and `jupyter notebook` would also take a random port,
+	// but unfortunately it won't report it.
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		klog.Errorf("Failed to get a random port available to use for jupyter notebook: %v", err)
+		return
+	}
+	jupyterPort = l.Addr().(*net.TCPAddr).Port
+	if err = l.Close(); err != nil {
+		klog.Errorf("Failed to close random port %d, temporarily opened to be used for jupyter notebook: %v", jupyterPort, err)
+		return
+	}
+
+	// Execute `jupyter notebook`.
+	args := []string{"notebook", "--no-browser",
+		fmt.Sprintf("--ServerApp.port=%d", jupyterPort),
+
+		// Allow access to `jupyterapp` javascript object, needed to
+		// command the notebook to execute all cells and save.
+		"--expose-app-in-browser",
+		"--ServerApp.allow_remote_access=false"}
+	for _, arg := range flagJupyterExtraFlags {
+		args = append(args, arg)
+	}
+	jupyterCmd = exec.Command(jupyterExecPath, args...)
+	if *flagJupyterDir != "" {
+		jupyterCmd.Dir = *flagJupyterDir
+	}
+
+	// Latches to report state of `jupyter notebook`
+	jupyterReady = common.NewLatch()
+	jupyterDone = common.NewLatch()
+	jupyterStop = common.NewLatch()
+
+	// Parses the output of jupyter for indication of Jupyters' readiness.
+	jpyOut := must.M1(jupyterCmd.StdoutPipe())
+	jpyErr := must.M1(jupyterCmd.StderrPipe())
+	const bufSize = 1024 * 1024
+	reJupyterAddress := regexp.MustCompile(
+		`\s+(http://127\.0\.0\.1:\d+/)tree\?token=(\w+)`)
+	//`Jupyter Server is listening on (http\+unix:.*\.sock/)tree\?token=(\w+)`)
+	pollingFn := func(reader io.Reader) {
+		var err error
+		var n int
+		for {
+			buf := make([]byte, bufSize)
+			n, err = reader.Read(buf)
+			if err != nil {
+				break
+			}
+			if *flagJupyterLog {
+				//fmt.Printf("[%q]\n", string(buf[:n]))
+				_, _ = os.Stdout.Write(buf[:n])
+			}
+			if !jupyterReady.Test() {
+				matches := reJupyterAddress.FindStringSubmatch(string(buf[:n]))
+				if len(matches) > 0 {
+					// We got the serving address.
+					klog.V(2).Infof("matches=%q", matches)
+					jupyterBaseUrl, jupyterToken = matches[1], matches[2]
+					klog.V(1).Infof("jupyter notebook: url=%q, token=%q", jupyterBaseUrl, jupyterToken)
+					jupyterReady.Trigger()
+				}
+			}
+		}
+		if !errors.Is(err, os.ErrClosed) && !errors.Is(err, io.EOF) {
+			klog.Errorf("Failed to read output of `jupyter notebook`: %v", err)
+		}
+	}
+	go pollingFn(jpyOut)
+	go pollingFn(jpyErr)
+
+	// Listen to jupyterStop and interrupts the program, if not yet finished.
+	go func() {
+		select {
+		case <-jupyterDone.WaitChan():
+			// Program finished, no need to stop it.
+			return
+		case <-jupyterStop.WaitChan():
+			// Continue to stop the program.
+		}
+		klog.Infof("Stopping Jupyter Notebook")
+		stopCmd := exec.Command(jupyterExecPath, "notebook",
+			"stop", fmt.Sprintf("%d", jupyterPort))
+		err := stopCmd.Run()
+		if err != nil {
+			klog.Errorf("Failed to stop jupyter notebook with %q: %v", stopCmd, err)
+		}
+
+		// Wait a few seconds, if notebooks doesn't die, kill it.
+		select {
+		case <-time.After(5 * time.Second):
+			klog.Errorf("Clean exit of `jupyter notebook` timed-out, killing it.")
+			err = jupyterCmd.Process.Kill()
+			if err != nil {
+				klog.Errorf("Killing (stopping) `jupyter notebook` failed!? Error: %v", err)
+			}
+		case <-jupyterDone.WaitChan():
+			// Clean exit.
+		}
+	}()
+
+	// Wait for the program to exit and trigger jupyterDone.
+	if err := jupyterCmd.Start(); err != nil {
+		jupyterDone.Trigger()
+		klog.Errorf("Failed to run %q: %v", jupyterCmd, err)
+		return
+	}
+	klog.Infof("Running: %s", jupyterCmd)
+	go func() {
+		err := jupyterCmd.Wait()
+		if err != nil && !jupyterStop.Test() {
+			klog.Errorf("`jupyter notebook` died, likely something failed. Consider re-running with --jupyter_log "+
+				"to output the contents of jupyter notebook. Error: %v", err)
+		}
+		jupyterDone.Trigger()
+	}()
+
+	select {
+	case <-jupyterDone.WaitChan():
+	case <-jupyterReady.WaitChan():
+		// Jupyter Notebook running, just continue below.
+	}
+}
+
+// Notebook instrumenting using go-rod.
+
+func executeNotebook(url string) {
+	page := rod.New().MustConnect().MustPage(url)
+	page.MustWaitStable()
+
+	if *flagConsoleLog {
+		// Listen for all events of console output.
+		go page.EachEvent(func(e *proto.RuntimeConsoleAPICalled) {
+			fmt.Printf("[console] %q\n", page.MustObjectsToJSON(e.Args))
+		})()
+	}
+
+	time.Sleep(3 * time.Second)
+	page.MustEval(`() => {
+	let jupyterapp = globalThis.jupyterapp;
+	jupyterapp.commandLinker._commands.execute("editmenu:clear-all", null).
+		then(() => { jupyterapp.commandLinker._commands.execute("runmenu:run-all", null); }).
+		then(() => { console.log("Finished executing!"); });
+}`)
+	klog.V(1).Infof("Started Javascript to execute cells, waiting to stabilize.")
+	page.MustWaitStable()
+	klog.V(1).Infof("page.MustWaitStable() finished.")
+
+	page.MustEval(`() => {
+	let jupyterapp = globalThis.jupyterapp;
+	jupyterapp.commandLinker._commands.execute("docmanager:save", null).
+		then(() => { console.log("Finished saving!"); });
+}`)
+	klog.V(1).Infof("Started Javascript to save notebook, waiting to stabilize.")
+	page.MustWaitStable()
+	klog.V(1).Infof("page.MustWaitStable() finished.")
+
+	if *flagScreenshot != "" {
+		klog.Infof("Screenshot to %q", *flagScreenshot)
+		page.MustScreenshot(*flagScreenshot)
+	}
+
+	//common.Pause()
+}
